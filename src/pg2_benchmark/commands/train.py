944b7c498ea9ccd0d34bdb35cb7096991ea9127e
tintinrevient <zhaobenben007@googlemail.com>
2025-05-15 13:01:47 +0200

    test pg2-benchmark

--- src/pg2_benchmark/commands/train.py
+++ src/pg2_benchmark/commands/train.py
@@ -1,6 +1,9 @@
+import os
 import typer
 from loguru import logger
+import tempfile
 from pg2_dataset.backends.records import RecordsDataset
+from pg2_benchmark.models.git_utils import git_clone
 from python_on_whales import docker
 
 app = typer.Typer(help="Training commands")
@@ -8,26 +11,61 @@ app = typer.Typer(help="Training commands")
 @app.command("model")
 def train(
     toml_file: str = typer.Option("--toml-file", help="Configuration TOML file"),
-):
+):  
+    
+    # 1. Load the dataset
     ds = RecordsDataset(
         toml_file=toml_file,
         include_records=True,
     )
+    
+    # 2. Load the parameters
+    logger.info(f"Loaded params: {ds.settings}")
 
-    df = ds.data_frame()
+    git_repo = ds.settings.assays['assay_one'].constants["git_repo"]
+    git_branch = ds.settings.assays['assay_one'].constants["git_branch"]
 
-    logger.info(f"loaded dataset: {df[:3]}")
-    logger.info(f"loaded params: {ds.settings.assays['assay_one'].constants}")
+    label_col_name = ds.settings.assays['assay_one'].target
+    sequence_length = ds.settings.assays['assay_one'].constants["sequence_length"]
+    n_components = ds.settings.assays['assay_one'].constants["n_components"]
 
-    # git.clone()
-    # docker.build()
+    with tempfile.TemporaryDirectory() as temp_dir:
+        # 3. Clone the git repository
+        git_clone(repo_url=git_repo, target_dir=temp_dir, branch_name=git_branch)
+        os.chdir(temp_dir)
 
-    docker.run(
-        image="pls-model", 
-        volumes=[("data", "/data")], 
-        remove=True,
-        command=["--input-file", "data/A0A140D2T1_ZIKV_Sourisseau_2019.parquet", "--output-file", "data/output.json"],
-    )
+        # 4. Build the Docker image
+        logger.info("Building Docker image...")
+        docker.build(
+            context_path="./",
+            tags=["pls-model:latest"],
+            load=True,
+        )
+
+        logger.info(f"{docker.image.list()}")
+
+        # 5. Load the dataset
+        os.makedirs(f"{temp_dir}/data", exist_ok=True)
+
+        ds.data_frame()[:7].to_csv(f"{temp_dir}/data/train.csv", index=False)
+        ds.data_frame()[7:10].to_csv(f"{temp_dir}/data/test.csv", index=False)
+        logger.info(f"Loaded data frame: {ds.data_frame()[:10]}")
+
+        # 6. Run the Docker container
+        logger.info("Running Docker container...")
+        docker.run(
+            image="pls-model:latest",
+            volumes=[(f"{temp_dir}/data", "/data")], 
+            remove=True,
+            command=[
+                "--train-file-path", "/data/train.csv",
+                "--test-file-path", "/data/test.csv",
+                "--sequence-length", sequence_length,
+                "--sequence-col-name", "sequence",
+                "--label-col-name", label_col_name,
+                "--n-components", n_components,
+            ],
+        )
 
 if __name__ == "__main__":
     app()
---
+++ src/pg2_benchmark/models/git_utils.py
@@ -0,0 +1,21 @@
+from loguru import logger
+import git
+
+def git_clone(repo_url: str, target_dir: str, branch_name: str) -> None:
+    """
+    Clone a git repository.
+    
+    Args:
+        repo_url (str): URL of the git repository to clone
+        target_dir (str): Directory where to clone the repository
+    """
+
+    
+    logger.info(f"Cloning repository from {repo_url} to {target_dir}...")
+    try:
+        repo = git.Repo.clone_from(url=repo_url, to_path=target_dir, branch=branch_name)
+        logger.info(f"Repository successfully cloned to {target_dir}")
+    
+    except git.GitCommandError as exc:
+        logger.error(f"Error cloning repository: {exc}")
+        return
