0185edf2c26e588f5133114fe952d067810cce3c
tintinrevient <zhaobenben007@googlemail.com>
2025-07-08 19:25:41 +0200

    Resolve merge conflict

--- models/pls/src/pg2_model_pls/main.py
+++ models/pls/src/pg2_model_pls/main.py
@@ -1,5 +1,5 @@
 import polars as pl
-from pathlib import Path
+from rich.console import Console
 from pg2_dataset.dataset import Manifest
 from pg2_dataset.splits.abstract_split_strategy import TrainTestValid
 from pg2_model_pls.manifest import Manifest as ModelManifest
@@ -12,13 +12,15 @@ app = typer.Typer(
     add_completion=True,
 )
 
+console = Console()
+
 
 @app.command()
 def predict(
     dataset_toml_file: str = typer.Option(help="Path to the dataset TOML file"),
     model_toml_file: str = typer.Option(help="Path to the model TOML file"),
 ):
-    typer.echo("Start...")
+    console.print(f"Loading {dataset_toml_file} and {model_toml_file}...")
 
     dataset_name = Manifest.from_path(dataset_toml_file).name
 
@@ -30,6 +32,10 @@ def predict(
         split=TrainTestValid.train,
     )
 
+    console.print(f"Loaded {len(train_Y)} training records.")
+
+    console.print("Start the training...")
+
     train_model(
         train_X=train_X,
         train_Y=train_Y,
@@ -37,16 +43,24 @@ def predict(
         model_path=model_path,
     )
 
+    console.print("Finished the training...")
+
     valid_X, valid_Y = load_x_and_y(
         dataset_toml_file=dataset_toml_file,
         split=TrainTestValid.valid,
     )
+
+    console.print(f"Loaded {len(valid_Y)} test records.")
+
+    console.print("Start the prediction...")
 
     pred_y = predict_model(
         test_X=valid_X,
         model_toml_file=model_toml_file,
         model_path=model_path,
     )
+
+    console.print("Finished the prediction...")
 
     df = pl.DataFrame(
         {
@@ -56,14 +70,15 @@ def predict(
         }
     )
 
-    df.write_csv(f"/output/{dataset_name}_{Path(model_name).stem}.csv")
+    df.write_csv(f"/output/{dataset_name}_{model_name}.csv")
+    console.print(f"Saved the metrics in CSV in output/{dataset_name}_{model_name}.csv")
 
-    typer.echo("Done.")
+    console.print("Done.")
 
 
 @app.command()
 def ping():
-    typer.echo("pong")
+    console.print("pong")
 
 
 if __name__ == "__main__":
