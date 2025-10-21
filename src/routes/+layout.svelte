3e26d9b89f7ba7edec9e2e657ee82997b4886052
tintinrevient <zhaobenben007@googlemail.com>
2025-10-21 14:52:08 +0200

    Add prettier

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
-	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
-	import { modelsStore } from '$lib/stores/models';
-	import { onMount } from 'svelte';
+  import "../app.css";
+  import favicon from "$lib/assets/favicon.svg";
+  import { modelsStore } from "$lib/stores/models";
+  import { onMount } from "svelte";
 
-	let { children } = $props();
+  let { children } = $props();
 
-	onMount(() => {
-		modelsStore.load();
-	});
+  onMount(() => {
+    modelsStore.load();
+  });
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+  <link rel="icon" href={favicon} />
 </svelte:head>
 
 {@render children?.()}
